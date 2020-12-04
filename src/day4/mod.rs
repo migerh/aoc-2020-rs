c5e9496d0ba618070d7f911b938e57c711e4f3b4
Michael Gerhaeuser <michael.gerhaeuser@methodpark.de>
2020-12-04 13:55:44 +0100

    Improve solution for day 4
    This new solution does not copy any input around and has a lot more
    error checking. There is still room for improvement of course:
    
    * The validators should not be created for every check, they could be
      static. But they are rather small (only a few bytes per validator) but
      still - they are thrown on the heap and not on the stack.
    * There is still an unguarded unwrap() in the height validator.

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -1,160 +1,163 @@
 use super::utils::ParseError;
 use regex::Regex;
 
-#[derive(Debug)]
-struct Passport {
-    birth_year: Option<String>,
-    issue_year: Option<String>,
-    expiration_year: Option<String>,
-    height: Option<String>,
-    hair_color: Option<String>,
-    eye_color: Option<String>,
-    passport_id: Option<String>,
-    country_id: Option<String>,
+fn get_validator(key: &str) -> Box<dyn FieldValidator> {
+    match key {
+        "byr" => Box::new(YearValidator { min: 1920, max: 2002 }),
+        "iyr" => Box::new(YearValidator { min: 2010, max: 2020 }),
+        "eyr" => Box::new(YearValidator { min: 2020, max: 2030 }),
+        "hgt" => Box::new(HeightValidator {}),
+        "hcl" => Box::new(PatternValidator { regex: Regex::new(r"^\#([0-9a-f]{6})$").unwrap() }),
+        "ecl" => Box::new(PatternValidator { regex: Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$").unwrap() }),
+        "pid" => Box::new(PatternValidator { regex: Regex::new(r"^\d{9}$").unwrap() }),
+        "cid" => Box::new(EmptyValidator { value: true }),
+        _ => Box::new(EmptyValidator { value: false }),
+    }
 }
 
-fn find_value_for_key<'a>(input: &str, key: &str) -> Option<String> {
-    let values = input.split(' ')
-        .map(|kv| kv.split(':').collect::<Vec<_>>())
-        .filter(|kv| kv[0] == key)
-        .collect::<Vec<_>>();
+struct Field<'a> {
+    identifier: &'a str,
+    value: &'a str,
+}
 
-    if values.is_empty() {
-        None
+impl<'a> Field<'a> {
+    pub fn new(
+        identifier: &'a str,
+        value: &'a str) -> Self {
+
+        Field { identifier, value }
+    }
+
+    pub fn is_valid(&'a self) -> bool {
+        // TODO: optimize this, we should not instantiate the validators every
+        // time
+        let validator = get_validator(self.identifier);
+        validator.is_valid(self)
+    }
+}
+
+trait FieldValidator {
+    fn is_valid(&self, field: &Field) -> bool;
+}
+
+struct EmptyValidator {
+    value: bool,
+}
+
+impl FieldValidator for EmptyValidator {
+    fn is_valid(&self, _: &Field) -> bool {
+        self.value
+    }
+}
+
+struct YearValidator {
+    min: i16,
+    max: i16,
+}
+
+impl FieldValidator for YearValidator {
+    fn is_valid(&self, field: &Field) -> bool {
+        field.value.parse::<i16>()
+            .map(|v| v >= self.min && v <= self.max)
+            .unwrap_or(false)
+    }
+}
+
+struct HeightValidator {
+}
+
+impl FieldValidator for HeightValidator {
+    fn is_valid(&self, field: &Field) -> bool {
+        lazy_static!{
+            static ref RE: Regex = Regex::new(r"(\d*)(cm|in)").unwrap();
+        }
+
+        if !RE.is_match(&field.value) {
+            return false;
+        }
+
+        RE.captures(&field.value)
+            .map(|cap| {
+                let unit = &cap[2];
+                // TODO: Get rid of this unwrap
+                let size = cap[1].parse::<u32>().unwrap();
+
+                if unit == "cm" && size >= 150 && size <= 193 {
+                    true
+                } else if unit == "in" && size >= 59 && size <= 76 {
+                    true
+                } else {
+                    false
+                }
+            })
+            .unwrap_or(false)
+    }
+}
+
+struct PatternValidator {
+    regex: Regex,
+}
+
+impl FieldValidator for PatternValidator {
+    fn is_valid(&self, field: &Field) -> bool {
+        self.regex.is_match(field.value)
+    }
+}
+
+struct Passport<'a> {
+    fields: Vec<Field<'a>>,
+}
+
+fn parse_field<'a>(field: &'a str) -> Result<Field<'a>, ParseError> {
+    let key_value = field.split(':').collect::<Vec<_>>();
+
+    if key_value.len() != 2 {
+        Err(ParseError::new("foo"))
     } else {
-        Some(values[0][1].to_owned())
+        Ok(Field::new(key_value[0], key_value[1]))
     }
 }
 
-impl From<&String> for Passport {
-    fn from(input: &String) -> Self {
-        let birth_year = find_value_for_key(input, "byr");
-        let issue_year = find_value_for_key(input, "iyr");
-        let expiration_year = find_value_for_key(input, "eyr");
-        let height = find_value_for_key(input, "hgt");
-        let hair_color = find_value_for_key(input, "hcl");
-        let eye_color = find_value_for_key(input, "ecl");
-        let passport_id = find_value_for_key(input, "pid");
-        let country_id = find_value_for_key(input, "cid");
+impl<'a> Passport<'a> {
+    fn from_str(line: &'a str) -> Result<Passport<'a>, ParseError> {
+        let fields = line.split(|c| c == ' ' || c == '\n')
+            .filter(|v| !v.is_empty())
+            .map(parse_field)
+            .collect::<Result<Vec<_>, ParseError>>()?;
 
-        Self { birth_year, issue_year, expiration_year, height, hair_color, eye_color, passport_id, country_id }
+        Ok(Passport { fields })
     }
 }
 
-impl Passport {
-    fn is_valid(self: &Self) -> bool {
-        self.birth_year.is_some() &&
-        self.issue_year.is_some() &&
-        self.expiration_year.is_some() &&
-        self.height.is_some() &&
-        self.hair_color.is_some() &&
-        self.eye_color.is_some() &&
-        self.passport_id.is_some()
+impl<'a> Passport<'a> {
+    fn has_required_fields(&self) -> bool {
+        let required_fields = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
+        required_fields.iter()
+            .all(|&k| self.fields.iter().any(|f| f.identifier == k))
     }
 
-    fn is_valid_for_2(self: &Self) -> Result<bool, ParseError> {
-        if !self.is_valid() {
-            return Ok(false);
-        }
-
-        if self.birth_year.is_some() {
-            let year = self.birth_year.as_ref().unwrap().parse::<i32>()?;
-            if year < 1920 || year > 2002 {
-                return Ok(false);
-            }
-        }
-
-        if self.issue_year.is_some() {
-            let year = self.issue_year.as_ref().unwrap().parse::<i32>()?;
-            if year < 2010 || year > 2020 {
-                return Ok(false);
-            }
-        }
-
-        if self.expiration_year.is_some() {
-            let year = self.expiration_year.as_ref().unwrap().parse::<i32>()?;
-            if year < 2020 || year > 2030 {
-                return Ok(false);
-            }
-        }
-
-        if self.height.is_some() {
-            lazy_static!{
-                static ref RE: Regex = Regex::new(r"(\d*)(cm|in)").unwrap();
-            }
-            if !RE.is_match(&self.height.as_ref().unwrap()) {
-                return Ok(false);
-            }
-            let cap = RE.captures(&self.height.as_ref().unwrap()).unwrap();
-            let size = cap[1].parse::<u32>()?;
-            let unit = cap[2].to_string();
-
-            if unit == "cm" && (size < 150 || size > 193) {
-                return Ok(false);
-            } else if unit == "in" && (size < 59 || size > 76) {
-                return Ok(false);
-            } else if unit != "cm" && unit != "in" {
-                return Ok(false);
-            }
-        }
-
-        if self.hair_color.is_some() {
-            lazy_static!{
-                static ref RE: Regex = Regex::new(r"^\#([0-9a-f]{6})$").unwrap();
-            }
-
-            if !RE.is_match(self.hair_color.as_ref().unwrap()) {
-                return Ok(false)
-            }
-        }
-
-        if self.eye_color.is_some() {
-            lazy_static!{
-                static ref RE: Regex = Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$").unwrap();
-            }
-
-            if !RE.is_match(self.eye_color.as_ref().unwrap()) {
-                return Ok(false)
-            }
-        }
-
-        if self.passport_id.is_some() {
-            lazy_static!{
-                static ref RE: Regex = Regex::new(r"^\d{9}$").unwrap();
-            }
-
-            if !RE.is_match(self.passport_id.as_ref().unwrap()) {
-                return Ok(false)
-            }
-        }
-
-        Ok(true)
+    fn is_valid(&self) -> bool {
+        self.fields
+            .iter()
+            .all(|v| v.is_valid())
     }
 }
 
-fn parse_input() -> Vec<Passport> {
+fn parse_input<'a>() -> Result<Vec<Passport<'a>>, ParseError> {
     let input = include_str!("./data/input.txt");
 
-    let mut raw_passports = vec![];
-    let mut passport_input = vec![];
-    for line in input.lines() {
-        if line.is_empty() {
-            raw_passports.push(passport_input.join(" "));
-            passport_input = vec![];
-        } else {
-            passport_input.push(line);
-        }
-    }
-    raw_passports.push(passport_input.join(" "));
-
-    raw_passports.iter().map(|v| Passport::from(v)).collect::<Vec<_>>()
+    input.split("\n\n")
+        .filter(|v| !v.is_empty())
+        .map(|v| Passport::from_str(v))
+        .collect::<Result<Vec<_>, ParseError>>()
 }
 
 pub fn problem1() -> Result<usize, ParseError> {
-    let input = parse_input();
+    let input = parse_input()?;
 
     let solution = input.iter()
-        .filter(|p| p.is_valid())
+        .filter(|p| p.has_required_fields())
         .count();
 
     println!("4/1: # of 'valid' passports: {}", solution);
@@ -162,18 +165,17 @@ pub fn problem1() -> Result<usize, ParseError> {
     Ok(solution)
 }
 
-pub fn problem2() -> Result<(), ParseError> {
-    let input = parse_input();
+pub fn problem2() -> Result<usize, ParseError> {
+    let input = parse_input()?;
 
     let solution = input.iter()
-        .map(|p| p.is_valid_for_2())
-        .collect::<Result<Vec<_>, ParseError>>()?
-        .iter()
-        .filter(|&&b| b)
+        .filter(|p| p.has_required_fields())
+        .filter(|p| p.is_valid())
         .count();
 
     println!("4/2: # of 'valid' passports: {}", solution);
-    Ok(())
+
+    Ok(solution)
 }
 
 #[cfg(test)]
@@ -181,10 +183,16 @@ mod test {
     use super::*;
 
     #[test]
-    pub fn example_1_1() {
+    pub fn valid_expiration_year_can_be_validated() {
+        let field = Field::new("eyr", "2020");
+
+        assert_eq!(true, field.is_valid());
     }
 
     #[test]
-    pub fn example_2_1() {
+    pub fn invalid_expiration_year_can_be_validated() {
+        let field = Field::new("eyr", "2015");
+
+        assert_eq!(false, field.is_valid());
     }
 }
