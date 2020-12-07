41e758129242efa23296b9a3364b7c8959c869a5
Michael Gerhaeuser <michael.gerhaeuser@methodpark.de>
2020-12-07 08:21:03 +0100

    Day 7
    It's not pretty, but it works. There's a loop in there that could be
    resolved by recursion.

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,12 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 mod utils;
 
 fn run() -> Result<(), utils::Error> {
-  day6::problem1()?;
-  day6::problem2()?;
+  day7::problem1()?;
+  day7::problem2()?;
 
   if false {
     day1::problem1();
@@ -32,6 +33,9 @@ fn run() -> Result<(), utils::Error> {
 
     day5::problem1()?;
     day5::problem2()?;
+
+    day6::problem1()?;
+    day6::problem2()?;
   }
   Ok(())
 }
