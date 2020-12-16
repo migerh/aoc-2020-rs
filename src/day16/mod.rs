b8718a9b9f7c8bd25c12aa4ddde3921f8d06ca93
Michael Gerhaeuser <michael.gerhaeuser@methodpark.de>
2020-12-16 08:14:18 +0100

    Day 16, Part 2, ugly

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -1,3 +1,6 @@
+use std::hash::Hasher;
+use std::hash::Hash;
+use std::collections::HashSet;
 use regex::Regex;
 use super::utils::ParseError;
 
@@ -17,6 +20,20 @@ impl Range {
 struct Rule {
     description: String,
     ranges: Vec<Range>,
+}
+
+impl PartialEq for Rule {
+    fn eq(&self, other: &Self) -> bool {
+        self.description == other.description
+    }
+}
+
+impl Eq for Rule {}
+
+impl Hash for Rule {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.description.hash(state);
+    }
 }
 
 impl Rule {
@@ -103,38 +120,111 @@ fn find_invalid_fields(rules: &Vec<Rule>, ticket: &Ticket) -> Vec<u32> {
 pub fn problem1() -> Result<(), ParseError> {
     let input = parse_input();
 
-    // println!("{:?}", input);
-
     let mut invalid_fields = vec![];
     for t in &input.nearby_tickets {
         let mut invalid_fields_for_ticket = find_invalid_fields(&input.rules, t);
         invalid_fields.append(&mut invalid_fields_for_ticket);
     }
 
-    // println!("{:?}", invalid_fields);
-    // let foo = find_invalid_fields(&input.rules, &input.nearby_tickets[2]);
-    // println!("first: {:?}", foo);
     let result: u32 = invalid_fields.iter().sum();
     println!("16/1: sum of invalid fields is {}", result);
 
     Ok(())
 }
 
+fn is_valid(rules: &Vec<Rule>, ticket: &Ticket) -> bool {
+    ticket.iter()
+        .all(|n| is_valid_for_some_field(rules, *n))
+}
+
+fn determine_valid_rules(rules: &Vec<Rule>, n: u32) -> HashSet<&Rule> {
+    rules.iter()
+        .filter(|r| r.is_valid(n))
+        .collect::<HashSet<_>>()
+}
+
+fn determine_valid_rules_2(rules: &Vec<Rule>, n: u32) -> HashSet<usize> {
+    rules.iter()
+        .enumerate()
+        .filter(|(_, r)| r.is_valid(n))
+        .map(|(i, _)| i)
+        .collect::<HashSet<_>>()
+}
+
 pub fn problem2() -> Result<(), ParseError> {
     let input = parse_input();
 
+    let valid_nearby_tickets = input.nearby_tickets.iter()
+        .filter(|t| is_valid(&input.rules, t))
+        .collect::<Vec<_>>();
+
+    println!("Valid tickets {:?}", valid_nearby_tickets);
+
+    // let's assume all tickets have the same length and it's the same as the
+    // of rules
+    let len = input.rules.len();
+    let mut candidates = vec![];
+
+    for i in 0..len {
+
+        let valid_rules = valid_nearby_tickets.iter()
+            .map(|t| determine_valid_rules(&input.rules, t[i]))
+            .collect::<Vec<_>>();
+
+        // println!("Valid rules for {} are {:?}", i, valid_rules);
+
+        let mut rules_iter = valid_rules.iter();
+        let mut one_rule = rules_iter.next().cloned().unwrap();
+        // println!("start: {:?}", one_rule);
+
+        for r in rules_iter {
+            // println!("intersect with {:?}", r);
+            one_rule = one_rule.intersection(&r).cloned().collect();
+        }
+
+        println!("one rule for {}: {:?}", i, one_rule);
+
+        // let one_rule_to_rule_them_all = valid_rules.iter().next()
+        //     .map(|initial| valid_rules
+        //         .iter()
+        //         .fold(initial, |acc, curr| acc.intersection(&curr).cloned().collect()));
+        // candidates.push(one_rule.into_iter().next().unwrap().clone());
+        candidates.push((i, one_rule));
+        println!("");
+    }
+
+    candidates.sort_by(|a, b| a.1.len().cmp(&b.1.len()));
+    let mut already_assigned = HashSet::new();
+
+    let mut columns = vec![];
+    for c in &mut candidates {
+        for a in &already_assigned {
+            c.1.remove(a);
+        }
+
+        if c.1.len() != 1 {
+            panic!("alles scheiße");
+        }
+
+        let next_column = c.1.iter().next().unwrap();
+        columns.push((c.0, next_column));
+        already_assigned.insert(*next_column);
+    }
+
+    let readable_candidates = columns.iter().map(|(i, c)| (i, c.description.clone())).collect::<Vec<_>>();
+    println!("Order of fields: {:?}", readable_candidates);
+
+    let bar = readable_candidates.iter()
+        .filter(|(_, d)| d.contains("departure"))
+        .map(|(i, _)| input.my_ticket[**i])
+        .collect::<Vec<_>>();
+    println!("rel fields in ticket: {:?}", bar);
+
+    let result: u64 = readable_candidates.iter()
+        .filter(|(_, d)| d.contains("departure"))
+        .map(|(i, _)| input.my_ticket[**i] as u64)
+        .product();
+    println!("16/2: sum of invalid fields is {}", result);
+
     Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    pub fn example_1_1() {
-    }
-
-    #[test]
-    pub fn example_2_1() {
-    }
 }
