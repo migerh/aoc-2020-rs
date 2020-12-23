cfd8aaa5f53803cd3a0927c2033deecc2fb25370
Michael Gerhaeuser <michael.gerhaeuser@methodpark.de>
2020-12-23 09:00:03 +0100

    Improve performance of part 2

--- src/day23/mod.rs
+++ src/day23/mod.rs
@@ -12,34 +12,42 @@ fn get_example() -> VecDeque<u64> {
 
 fn pick_three(cups: &mut VecDeque<u64>) -> Vec<u64> {
     let first = cups.pop_front().unwrap();
-    let mut three = vec![];
-    for _ in 0..3 {
-        three.push(cups.pop_front().unwrap());
-    }
+    let three = vec![
+        cups.pop_front().unwrap(),
+        cups.pop_front().unwrap(),
+        cups.pop_front().unwrap()
+    ];
+    // for _ in 0..3 {
+    //     three.push(cups.pop_front().unwrap());
+    // }
     cups.push_front(first);
     three
 }
 
-fn rotate_to_destination(cups: &mut VecDeque<u64>) {
-    let mut remaining = cups.iter().cloned().collect::<Vec<_>>();
-    remaining.sort_by(|a, b| b.cmp(a));
-    let first = cups[0];
-
-    let first_position = remaining.iter().position(|v| *v == first).unwrap();
-    let destination_position = (first_position + 1) % remaining.len();
-    let destination = remaining[destination_position];
-
-    while cups[0] != destination {
-        cups.rotate_left(1);
+fn get_destination(max: u64, destination: u64) -> u64 {
+    if destination == 1 {
+        max as u64
+    } else {
+        destination - 1
     }
 }
 
-fn push_three(cups: &mut VecDeque<u64>, three: &Vec<u64>) {
-    let first = cups.pop_front().unwrap();
-    for t in three.iter().rev() {
-        cups.push_front(*t);
+fn insert_three(cups: &mut VecDeque<u64>, max: u64, three: &Vec<u64>) {
+    let current = cups[0];
+
+    let mut destination = get_destination(max, current);
+    while three.contains(&destination) {
+        destination = get_destination(max, destination);
     }
-    cups.push_front(first);
+    // println!("destination: {}", destination);
+
+    let destination_position = cups.iter().position(|v| *v == destination).unwrap();
+    cups.insert(destination_position + 1, three[2]);
+    cups.insert(destination_position + 1, three[1]);
+    cups.insert(destination_position + 1, three[0]);
+    // for t in three.iter().rev() {
+    //     cups.insert(destination_position + 1, *t);
+    // }
 }
 
 fn rotate_to_new_current(cups: &mut VecDeque<u64>, current: u64) {
@@ -50,13 +58,11 @@ fn rotate_to_new_current(cups: &mut VecDeque<u64>, current: u64) {
     cups.push_back(c);
 }
 
-fn turn(cups: &mut VecDeque<u64>) {
+fn turn(cups: &mut VecDeque<u64>, max: u64) {
     let current = cups[0];
     let three = pick_three(cups);
     // println!("pick up: {:?}", three);
-    rotate_to_destination(cups);
-    // println!("destination: {}", cups[0]);
-    push_three(cups, &three);
+    insert_three(cups, max, &three);
     rotate_to_new_current(cups, current);
 }
 
@@ -77,7 +83,7 @@ pub fn problem1() -> Result<(), ParseError> {
     for t in 0..100 {
         println!("--- move {} ---", t + 1);
         println!("cups: {:?}", cups);
-        turn(&mut cups);
+        turn(&mut cups, 9);
         println!("");
     }
 
@@ -95,12 +101,13 @@ pub fn problem2() -> Result<(), ParseError> {
     }
 
     for t in 0..10_000_000 {
+    // for t in 0..10 {
         if t % 10_000 == 0 {
             println!("move {}", t);
         }
         // println!("--- move {} ---", t + 1);
         // println!("cups: {:?}", cups);
-        turn(&mut cups);
+        turn(&mut cups, 1_000_000);
         // println!("");
     }
     rotate_to_1(&mut cups);
