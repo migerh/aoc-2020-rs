2acd120464e28f05a393885f7c8c331d1e9bc838
Michael Gerhaeuser <michael.gerhaeuser@methodpark.de>
2020-12-24 06:53:23 +0100

    Day 24, Part 1

--- src/day24/mod.rs
+++ src/day24/mod.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
+use std::ops::Add;
 use super::utils::ParseError;
 
 #[derive(Debug)]
-enum Directions {
+enum Direction {
     NorthEast,
     East,
     SouthEast,
@@ -10,7 +12,7 @@ enum Directions {
     NorthWest,
 }
 
-fn parse_line(s: &str) -> Vec<Directions> {
+fn parse_line(s: &str) -> Vec<Direction> {
     let mut i = s.chars().peekable();
     let mut result = vec![];
 
@@ -20,25 +22,25 @@ fn parse_line(s: &str) -> Vec<Directions> {
         let d = match (c, p) {
             ('s', Some('e')) => {
                 i.next();
-                Directions::SouthEast
+                Direction::SouthEast
             },
             ('s', Some('w')) => {
                 i.next();
-                Directions::SouthWest
+                Direction::SouthWest
             },
             ('w', _) => {
-                Directions::West
+                Direction::West
             },
             ('e', _) => {
-                Directions::East
+                Direction::East
             },
             ('n', Some('e')) => {
                 i.next();
-                Directions::NorthEast
+                Direction::NorthEast
             },
             ('n', Some('w')) => {
                 i.next();
-                Directions::NorthWest
+                Direction::NorthWest
             },
             (v1, v2) => panic!(format!("Encountered unexpected pair of input chars: '{}' and '{:?}'", v1, v2))
         };
@@ -49,8 +51,8 @@ fn parse_line(s: &str) -> Vec<Directions> {
     result
 }
 
-fn parse_input() -> Vec<Vec<Directions>> {
-    let input = include_str!("./data/example.txt");
+fn parse_input() -> Vec<Vec<Direction>> {
+    let input = include_str!("./data/input.txt");
     input
         .lines()
         .filter(|v| *v != "")
@@ -58,10 +60,72 @@ fn parse_input() -> Vec<Vec<Directions>> {
         .collect::<Vec<_>>()
 }
 
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Coords {
+    x: i64,
+    y: i64,
+    z: i64,
+}
+
+impl Coords {
+    pub fn new(x: i64, y: i64, z: i64) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn zeroes() -> Self {
+        Self { x: 0, y: 0, z: 0 }
+    }
+
+    pub fn from_direction(d: &Direction) -> Self {
+        let (x, y, z) = match d {
+            Direction::NorthEast => (1, -1, 0),
+            Direction::East => (1, 0, -1),
+            Direction::SouthEast => (0, 1, -1),
+            Direction::SouthWest => (-1, 1, 0),
+            Direction::West => (-1, 0, 1),
+            Direction::NorthWest => (0, -1, 1),
+        };
+
+        Self { x, y, z }
+    }
+}
+
+impl Add for Coords {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+fn get_tile(instructions: &Vec<Direction>) -> Coords {
+    let tile = instructions.iter()
+        .map(|d| Coords::from_direction(d))
+        .fold(Coords::zeroes(), |a, c| a + c);
+
+    tile
+}
+
 pub fn problem1() -> Result<(), ParseError> {
     let input = parse_input();
 
-    println!("{:?}", input[2]);
+    let mut floor: HashMap<Coords, bool> = HashMap::new();
+    for tile in input.iter() {
+        let coords = get_tile(tile);
+        floor.entry(coords)
+            .and_modify(|v| *v = !*v)
+            .or_insert(true);
+    }
+
+    let result = floor.iter()
+        .filter(|(_, v)| **v)
+        .count();
+
+    println!("24/1: # of flipped tiles: {}", result);
 
     Ok(())
 }
